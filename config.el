fc6a4922fdd8724a7d98d22bf6974a7e3eebe7cd
petr-tik <petr-tik@users.noreply.github.com>
2020-11-05 00:17:13 +0000

    lsp
    add tramp support for pyls
    enable lsp-format-buffer in python mode
    recover ivy without childframe
    
    add custom clangd functions

--- config.el
+++ config.el
@@ -68,7 +68,13 @@
 
 (after! lsp-mode
   (setq lsp-enable-folding t)
-  (setq lsp-clients-clangd-args '("--clang-tidy" "-j=12" "--log=verbose" "--pch-storage=memory" "--query-driver=/usr/bin/c++")))
+  (setq lsp-clients-clangd-args '("--clang-tidy" "-j=12" "--log=verbose" "--pch-storage=memory" "--query-driver=/usr/bin/c++"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
+                    :major-modes '(python-mode)
+                    :remote? t
+                    :server-id 'pyls-remote))
+  )
 
 (map! :leader
       :desc "eshell in project root"
@@ -87,7 +93,7 @@
       #'counsel-fzf)
 
 (map! :after lsp-mode
-      :map (rustic-mode-map c++-mode-map)
+      :map (rustic-mode-map c++-mode-map python-mode-map)
       :localleader
       :nv "=" #'lsp-format-buffer)
 
@@ -97,5 +103,53 @@
   (setq magit-bury-buffer-function #'magit-mode-quit-window))
 
 
+(use-package! tramp
+  :config
+  (add-to-list 'tramp-remote-path "~/.local/bin/")
+  (add-to-list 'tramp-connection-properties
+               (list (regexp-quote "/ssh:petr_tik@192.*:")
+                     "remote-shell" "/bin/bash")))
+
+
+
 (custom-set-faces!
   '(font-lock-comment-face :foreground "#C2B493" :slant italic))
+
+(defun lsp-clangd-update-args-with-compile-commands ()
+  "Unless --compile-commands-dir is already set, run a potentially interactive search for the directory that contains compile_commands.json.
+
+  Then return either the default args or the modified args"
+  (if (-first
+       ;; lsp-client-clangd-args might already lists a "--compile-commands-dir=build/" and will fail if we pass another one
+       ;; trust the user - if it's already set, don't mess around
+       (lambda (arg) (string-prefix-p "--compile-commands-dir" arg))
+       lsp-clients-clangd-args)
+      ((lsp--info "compilation_commands_dir set in clangd-args")
+       lsp-clients-clangd-args)
+    (if-let (found-dir (lsp-clangd-find-compile-commands-dir))
+        ;; TODO expensive to create a list from 1 element - how to append atom to list
+        (append lsp-clients-clangd-args (list (concat "--compile-commands-dir=" found-dir)))
+      lsp-clients-clangd-args)
+))
+
+
+(defun lsp-clangd-find-compile-commands-dir ()
+  "Searches for directories that contain the compile_commands.json and returns the full directory path or nil
+
+  If multiple compile_commands.json files are found in the project directory tree - ask the user to choose"
+  ;; TODO lsp-workspace-root won't find anything at the start of the workspace
+  (let ((candidate-dirs (directory-files-recursively (lsp-workspace-root) "compile_commands.json")))
+    (pcase (length candidate-dirs)
+          ;; TODO can I lsp--warn-user-in-message-and-log at the same time
+      (`0 (lsp--warn "Cannot find compile_commands.json - intellisense might not work")
+          nil)
+      ;; if you find 1 only - assume it's correct and strip the dirname from the full path
+      (`1 (file-name-directory (car candidate-dirs)))
+      ;; if you have several - ask the user to choose with lsp--completing-read
+      ;; TODO if Ctrl-G from inside this completing read still falls to default lsp-clients-clangd-args
+      (_ (file-name-directory (lsp--completing-read
+           "Found compile_commands.json in several directories, which one do you want to use?"
+           candidate-dirs
+           (lambda (candidate) candidate))))
+      )
+    ))
